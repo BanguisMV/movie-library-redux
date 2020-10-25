91759442349e87efba0dca4c3179a40834b5cb40
Marky <MarkVergelBanguis@gmail.com>
2020-10-25 12:28:38 -0700

    ...

--- src/components/movies/NotFound.js
+++ src/components/movies/NotFound.js
@@ -1,9 +1,36 @@
 import React from 'react'
 import notFound from '../../404.jpg';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import HomeIcon from '@material-ui/icons/Home';
+import { useHistory } from 'react-router-dom';
+const useStyles = makeStyles((theme) => ({
+    root: {
+            color:'#441f8a',
+            textAlign:'center',
+            padding:'2rem',
+            backgroundColor:'inherit',
+    },
+    button: {
+      marginTop:'10rem'
+    },
+  }));
 const NotFound = () => {
+    const history = useHistory()
+    const classes = useStyles();
     return (
-        <div className='errorPage'>
-           <img src={notFound} alt="404" style={{width:'100%', height:'100%'}} />
+        <div className={classes.root}>
+           <h1 style={{textTransform:'uppercase'}}>404 Page not found</h1>
+           <Button
+                variant="contained"
+                color="primary"
+                onClick={() => history.push('/')}
+                style={{ backgroundColor:'#441f8a' }}
+                className={classes.button}
+                startIcon={<HomeIcon />}
+            >
+                Home
+            </Button>
         </div>
     )
 }
